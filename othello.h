0d4dc56f40ed9b528602feb6de2b17965a98e875
Omar Khodr <omar.khoder2000@gmail.com>
2020-11-25 15:25:11 +0200

    Remove old functions, Add comments to header file

--- othello.h
+++ othello.h
@@ -12,16 +12,13 @@ public:
 
     QVector<QVector<int>*>* board;
     int turn;
-    int moveCounter;
 
-    QVector<QPair<int, int>*>* playableCells();
-    bool canPlay(int x, int y);
-    bool canPlay1(int x, int y);
-    int play(int x, int y); // returns the player number whose number is next.
-    int play1(int x, int y); // returns the player number whose number is next.
-    bool isGameOver();
-    int winner();
-    void print();
+    QVector<QPair<int, int>*>* playableCells(); // Returns vector of cells where current player can place his piece.
+    bool canPlay(int x, int y); // DONT'T USE: returns true if current player can place a piece in this cell, false otherwise.
+    int play(int x, int y); // Places piece at cell, assumes cell is valid. Returns the player number whose turn is next.
+    bool isGameOver(); // Returns true if no more moves can be made by either players, false otherwise.
+    int winner(); // To be called after isGameOver() returns true. Returns player with higher piece count.
+    void print(); // CONSOLE ONLY: prints board on console.
 
 };
 
